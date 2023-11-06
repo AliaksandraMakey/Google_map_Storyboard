bbdf214a9b6141a0d9e4ce9ed0517477303a6894
Alexandra Makey <aliaksandra.makey@gmail.com>
2023-11-06 14:01:13 +0700

    Initial Commit

---
+++ Google_Map_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google_Map_Storyboard
+//
+//  Created by Александра Макей on 06.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
