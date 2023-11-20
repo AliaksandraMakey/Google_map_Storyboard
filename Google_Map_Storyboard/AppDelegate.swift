b08b9bcbc62ec538bbac4b82f31dbd6b826363cf
Alexandra Makey <aliaksandra.makey@gmail.com>
2023-11-20 19:48:51 +0700

    First commit

--- Google_Map_Storyboard/AppDelegate.swift
+++ Google_Map_Storyboard/AppDelegate.swift
@@ -6,19 +6,19 @@
 //
 
 import UIKit
+import GoogleMaps
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Setting provide APIKey for GoogleMaps
+        GMSServices.provideAPIKey("")
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Google_Map_Storyboard' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Google_Map_Storyboard
+  pod 'GoogleMaps'
+
+
+end
